177bc0f1f8de6b9bf4ed7fb5753240e96583b653
Kostas Minaidis <minaidisk@gmail.com>
2026-09-17 19:55:01 +0300

    Implement Base64 Encoding/Decoding

--- src/base64.ts
+++ src/base64.ts
@@ -1,5 +1,56 @@
-import vscode from 'vscode';
+import * as vscode from 'vscode';
+import Window = vscode.window;
+import Range = vscode.Range;
+import Selection = vscode.Selection;
+import TextDocument = vscode.TextDocument;
+import TextEditor = vscode.TextEditor;
 
-export default function activateBase64Extension(context: vscode.ExtensionContext){
+function base64Encode(e: TextEditor, d: TextDocument, sel: Selection[]) {
+  e.edit(function (edit) {
+    sel.forEach(function (s) {
+      let txt: string = d.getText(new Range(s.start, s.end));
+      let b: Buffer = new Buffer(txt);
+      edit.replace(s, b.toString('base64'));
+    });
+  });
+}
+
+function base64Decode(e: TextEditor, d: TextDocument, sel: Selection[]) {
+  e.edit(function (edit) {
+    sel.forEach(function (s) {
+      let txt: string = d.getText(new Range(s.start, s.end));
+      let b: Buffer = new Buffer(txt, 'base64');
+      edit.replace(s, b.toString());
+    });
+  });
+}
+
+export default function activateBase64Extension(context: vscode.ExtensionContext) {
+
+  let encode = vscode.commands.registerCommand('vscode-wdx-development-extension.base64Encode', () => {
+    let e = Window.activeTextEditor;
+    if (!e){
+      return;
+    }
+    let d = e.document;
+    let sel = e.selections;
+    // @ts-ignore
+    base64Encode(e, d, sel);
+  });
+
+  let decode = vscode.commands.registerCommand('vscode-wdx-development-extension.base64Decode', () => {
+    let e = Window.activeTextEditor;
+    if (!e){
+      return;
+    }
+    let d = e.document;
+    let sel = e.selections;
+    // @ts-ignore
+    base64Decode(e, d, sel);
+  });
+
+  context.subscriptions.push(encode);
+  context.subscriptions.push(decode);
 
 }
+
